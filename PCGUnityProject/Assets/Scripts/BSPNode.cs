05b38cc5751a3477af61792aeb8d9e4d81384067
Jorge Palacios <pctroll@gmail.com>
2018-10-23 04:27:59 -0700

    Initial commit

---
+++ PCGUnityProject/Assets/Scripts/BSPNode.cs
@@ -0,0 +1,152 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public enum BSPNodeType
+{
+    Branch, Leaf
+}
+
+public class BSPNode
+{
+    private BSPNodeType _type;
+    private SplitType _split;
+    public BSPNodeType Type
+    {
+        get { return _type; }
+    }
+    public SplitType SplitDirection
+    {
+        get { return _split; }
+    }
+    public Rect area;
+    public Rect block;
+    public Dungeon dungeon;
+    public BSPNode left;
+    public BSPNode right;
+
+    public static int cutVal = 10;
+
+    //public int depth;
+
+    public BSPNode(Rect area, Dungeon dungeon)
+    {
+        this.area = area;
+        this.dungeon = dungeon;
+        this._type = BSPNodeType.Branch;
+        this.block = Rect.zero;
+        this.left = null;
+        this.right = null;
+        //Debug.Log("type: " + type);
+        // this.type = Mathf.Clamp(type, 1, 3);
+    }
+
+    public void Split()
+    {
+        Debug.Log("w:" + area.width + "  h:" + area.height);
+        int max = (int)Mathf.Max(area.width, area.height);
+        Debug.Log("min: " + max);
+        Debug.Log("cutVal:" + cutVal);
+        if (max <= cutVal)
+        {
+            _type = BSPNodeType.Leaf;
+            block = CreateBlock(area);
+            return;
+        }
+        _split = SplitType.Horizontal;
+        if (area.width == area.height)
+        {
+            int randNum = Random.Range(0, 99);
+            if (randNum % 2 == 0)
+                _split = SplitType.Vertical;
+        }
+        else if (area.width > area.height)
+            _split = SplitType.Vertical;
+
+        Rect[] areas = new Rect[2];
+        // bool isHeightMax = area.height >= area.width;
+        float divider, cut;//, cutTop;
+        divider = Random.Range(0.4f, 0.6f);
+        divider = 0.5f;
+        if (_split == SplitType.Horizontal)
+        {
+            cut = Mathf.RoundToInt(area.height * divider);
+            areas[0].xMin = area.xMin;
+            areas[0].yMin = area.yMin;
+            areas[0].xMax = area.xMax;
+            areas[0].yMax = cut;
+
+            areas[1].xMin = area.xMin;
+            areas[1].yMin = cut;
+            areas[1].xMax = area.xMax;
+            areas[1].yMax = area.yMax;
+        }
+        else
+        {
+            cut = Mathf.RoundToInt(area.width * divider);
+            areas[0].xMin = area.xMin;
+            areas[0].yMin = area.yMin;
+            areas[0].yMax = area.yMax;
+            areas[0].xMax = cut;
+
+            areas[1].xMin = cut;
+            areas[1].yMin = area.yMin;
+            areas[1].xMax = area.xMax;
+            areas[1].yMax = area.yMax;
+        }
+
+        left = new BSPNode(areas[0], dungeon);
+        right = new BSPNode(areas[1], dungeon);
+        // left.Split();
+        // right.Split();
+        dungeon.AddNode(left);
+        dungeon.AddNode(right);
+    }
+
+    // public void Split(Dungeon.Split splitCall)
+    // {
+    //     //int val = (int)(Mathf.Max(area.width, area.height));
+    //     //if (val < cutVal)
+    //     Blob b = splitCall(area);
+    //     if (b == null)
+    //     {
+    //         Debug.Log("No need to cut");
+    //         block = CreateBlock(area);
+    //         dungeon.BlockToGrid(this);
+    //         //dungeon.BlockToGrid(block);
+    //         return;
+    //     }
+    //     //Debug.Log("Cutting");
+    //     //Blob b = splitCall(area);
+    //     left = new BSPNode(b.areaLeft, dungeon);
+    //     right = new BSPNode(b.areaRight, dungeon);
+    //     left.Split(splitCall);
+    //     right.Split(splitCall);
+    //     //dungeon.BindBlocks(left.area, right.area, b.splitType);
+    // }
+
+    public static Rect CreateBlock(Rect area)
+    {
+        Rect block = new Rect
+        {
+            xMin = area.xMin + 1f,
+            yMin = area.yMin + 1f,
+            xMax = area.xMax - 1f,
+            yMax = area.yMax - 1f
+        };
+        return block;
+    }
+
+}
+
+public enum SplitType
+{
+    Horizontal, Vertical
+}
+
+[System.Serializable]
+public class Blob
+{
+    public SplitType splitType;
+    public Rect areaLeft;
+    public Rect areaRight;
+}
---
+++ PCGUnityProject/Assets/Scripts/Dungeon.cs
@@ -0,0 +1,209 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Dungeon : MonoBehaviour
+{
+    [Range(3, 200)]
+    public int maxHeight;
+    [Range(3, 200)]
+    public int maxWidth;
+    /// <summary>
+    /// Minimum Acceptable Size
+    /// </summary>
+    /// [
+    [Range(2, 100)]
+    public int minAcceptSize;
+    /// <summary>
+    /// Dungeon area
+    /// </summary>
+    private Rect area;
+    public Dictionary<int, List<BSPNode>> tree;
+    public HashSet<BSPNode> leaves;
+
+    public delegate Blob Split(Rect area);
+    public Split splitCall;
+
+    public BSPNode root;
+
+    public int[,] grid;
+
+    private Queue<BSPNode> _queue;
+
+    public void Init()
+    {
+        BSPNode.cutVal = minAcceptSize;
+        // print("Dungeon.Init");
+        area = new Rect();
+        area.xMin = 0;
+        area.yMin = 0;
+        area.xMax = maxWidth;
+        area.yMax = maxHeight;
+        grid = new int[maxHeight, maxWidth];
+        root = new BSPNode(area, this);
+        _queue = new Queue<BSPNode>();
+    }
+
+    public void AddNode(BSPNode node)
+    {
+        _queue.Enqueue(node);
+    }
+    public void BindBlocks(Rect a, Rect b, SplitType split)
+    {
+        //print("BindBlocks");
+        int originX, originY, targetX, targetY;
+        if (split == SplitType.Horizontal)
+        {
+            originX = (int)Random.Range(a.xMin + 1, a.xMax - 1);
+            originY = (int)a.center.x;
+            targetX = originX;
+            targetY = (int)b.center.y;
+        }
+        else
+        {
+            originX = (int)a.center.x;
+            originY = (int)Random.Range(a.yMin + 1, a.yMax - 1);
+            targetX = (int)b.center.x;
+            targetY = originY;
+        }
+        //print("origin: (" + originX + "," + originY + ")");
+        //print("target: (" + targetX + "," + targetY + ")");
+
+        int i, j;
+        for (i = originY; i <= targetY; i++)
+        {
+            // print("i: " + i);
+            for (j = originX; j <= targetX; j++)
+            {
+                // print("j: " + j);
+                // print("(" + j + "," + i + ")");
+                if (grid[i,j] == 0)
+                    grid[i, j] = 1;
+            }
+        }
+
+    }
+
+    public void BlockToGrid(BSPNode node)
+    {
+        // print("-------------");
+        // print("BlockToGrid");
+        // print("area");
+        Rect area = node.area;
+        Rect block = node.block;
+        // print(area);
+        // print("xMin:" + area.xMin + ", yMin:" + area.yMin + ", xMax:" + area.xMax + ", yMax:" + area.yMax);
+        // print("block");
+        // print(node.block);
+        // print("xMin:" + block.xMin + ", yMin:" + block.yMin + ", xMax:" + block.xMax + ", yMax:" + block.yMax);
+        int i, j, x, y, w, h;
+        x = (int)node.block.xMin;
+        y = (int)node.block.yMin;
+        w = (int)node.block.xMax;
+        h = (int)node.block.yMax;
+        //print("i:" + (int)block.y + " j:" + (int)block.x + " w:" + w + " h:" + h);
+        for (i = y; i < h; i++)
+        {
+            for (j = x; j < w; j++)
+            {
+                grid[i, j] = 1;
+                //print("x:" + j + ", y:" + i + " - " + node.type);
+            }
+        }
+    }
+
+
+    public void Build()
+    {
+        Init();
+        _queue.Enqueue(root);
+        while (_queue.Count != 0)
+        {
+            BSPNode n = _queue.Dequeue();
+            n.Split();
+        }
+        // Raster(root);
+    }
+
+    private void Raster(BSPNode node)
+    {
+        if (node == null)
+            return;
+        if (node.left == null && node.right == null)
+        {
+            BlockToGrid(node);
+            return;
+        }
+        Raster(node.left);
+        Raster(node.right);
+    }
+
+    private void Awake()
+    {
+        tree = new Dictionary<int, List<BSPNode>>();
+        leaves = new HashSet<BSPNode>();
+    }
+
+    public Blob SplitArea(Rect area)
+    {
+        //print("SplitArea");
+        int val = (int)Mathf.Max(area.width, area.height);
+        // print("area: " + area);
+        // Debug.Log("val: " + val);
+        // Debug.Log("min: " + minAcceptSize);
+        if (val < minAcceptSize)
+        {
+            // print("stop splitting");
+            return null;
+        }
+        // print("start splitting");
+        //print("SplittingNode");
+        //print("area");
+        //print(area);
+        //print("----");
+        Rect[] areas = new Rect[2];
+        bool isHeightMax = area.height >= area.width;
+        float divider, cut;//, cutTop;
+        divider = Random.Range(0.4f, 0.6f);
+        divider = 0.5f;
+        Blob blob = new Blob();
+        if (isHeightMax)
+        {
+            blob.splitType = SplitType.Horizontal;
+            cut = Mathf.RoundToInt(area.height * divider);
+            //cutTop = Mathf.CeilToInt(area.height * divider);
+
+            areas[0].x = area.x;
+            areas[0].y = area.y;
+            areas[0].height = cut;
+            areas[0].width = area.width;
+
+            areas[1].x = area.x;
+            areas[1].y = cut+1;
+            areas[1].height = area.height - cut;
+            areas[1].width = area.width;
+        }
+        else
+        {
+            blob.splitType = SplitType.Vertical;
+            cut = Mathf.FloorToInt(area.width * divider);
+            //cutTop = Mathf.CeilToInt(area.width * divider);
+
+            areas[0].x = area.x;
+            areas[0].y = area.y;
+            areas[0].height = area.height;
+            areas[0].width = cut;
+
+            areas[1].x = cut=1;
+            areas[1].y = area.y;
+            areas[1].height = area.height;
+            areas[1].width = area.width - cut;
+        }
+        print("area 0: " + areas[0].ToString());
+        print("area 1: " + areas[1].ToString());
+        blob.areaLeft = areas[0];
+        blob.areaRight = areas[1];
+        return blob;
+    }
+
+
+}
